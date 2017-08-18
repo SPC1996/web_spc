2e0a5b7a7276fc9265d072dd36e0a1d01c35d72c
SPC1996 <1594475695@qq.com>
2017-08-18 10:43:55 +0800

    simple use spring security

--- src/main/java/com/keessi/web/Application.java
+++ src/main/java/com/keessi/web/Application.java
@@ -1,16 +1,39 @@
 package com.keessi.web;
 
+import com.keessi.web.service.SecureService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Arrays;
 
 @SpringBootApplication
-public class Application {
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class Application implements CommandLineRunner {
+    private final SecureService service;
+
+    @Autowired
+    public Application(SecureService service) {
+        this.service = service;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new UsernamePasswordAuthenticationToken("user", "N/A",
+                        AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")));
+        try {
+            System.out.println(this.service.secure());
+        } finally {
+            SecurityContextHolder.clearContext();
+        }
     }
 }
---
+++ src/main/java/com/keessi/web/service/SecureService.java
@@ -0,0 +1,23 @@
+package com.keessi.web.service;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecureService {
+    @Secured("ROLE_USER")
+    public String secure() {
+        return "Hello Security";
+    }
+
+    @PreAuthorize("true")
+    public String authorized() {
+        return "Hello World";
+    }
+
+    @PreAuthorize("false")
+    public String denied() {
+        return "Goodbye World";
+    }
+}
